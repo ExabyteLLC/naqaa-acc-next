30c0026412150624a671c8393fc01a191b2104ab
Bavly Ashraf <64695350+Bavly001@users.noreply.github.com>
2024-03-19 10:22:51 +0200

    1

--- src/models/fetch.jsx
+++ src/models/fetch.jsx
@@ -1,3 +1,5 @@
+import Cookies from "universal-cookie";
+
 const myFetch = async (
   url,
   {
@@ -10,8 +12,7 @@ const myFetch = async (
 ) => {
   const URL =
     "https://development.exabyte-eg.com/explorer/__/elshall/BeyondQuality-Api/";
-  const uId =
-    "?UID=bmcyaC82T0NPNkYwS080Mlk4RT06NDVZRDMxVnNmV3RNSW54aWcrWW9IUT09=";
+  const uId = "?UID=" + (await GetUID());
   const res = await fetch(URL + url + uId, {
     method: method === "GET" && body ? "POST" : method,
     body,
@@ -34,3 +35,38 @@ const myFetch = async (
   return { res, data };
 };
 export default myFetch;
+
+const cookies = new Cookies(null, { path: "/" });
+async function GetUID() {
+  var now = new Date();
+  now.setHours(now.getHours() + 1);
+  // get from cookie
+  var CUID = cookies.get("UID");
+  if (CUID) {
+    cookies.set("UID", CUID, { expires: now });
+    return CUID;
+  }
+  // not in cookie then run login
+  var fd = new FormData();
+  fd.set("username", "mahmoudelshall292@gmail.com");
+  fd.set("password", "123456");
+  const URL =
+    "https://development.exabyte-eg.com/explorer/__/elshall/BeyondQuality-Api/";
+  const res = await fetch(URL + "admin/access/login", {
+    method: "POST",
+    body: fd,
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    console.log(`${res.status}: request error!`);
+    return;
+  }
+  if (data.status !== 200) {
+    console.log(`${data.statusText}: ${data.message}`);
+    return;
+  }
+  // put in cookie
+  cookies.set("UID", data.data.UID, { expires: now });
+  // return
+  return data.data.UID;
+}
